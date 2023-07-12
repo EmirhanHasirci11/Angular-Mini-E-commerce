db76ce3c949c49d850a4298a096bbeea9d6f27db
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-12 15:28:59 +0300

    Spinner implemented

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
-  template:`<router-outlet></router-outlet>`,
+  template:`
+  <router-outlet></router-outlet>
+  <ngx-spinner bdColor = "rgba(0, 0, 0, 0.8)" size = "medium" color = "#fff" type = "ball-clip-rotate" [fullScreen] = "true"><p style="color: white" > Loading... </p></ngx-spinner>
+  `,
   standalone:true,
-  imports:[RouterModule]
+  imports:[RouterModule,NgxSpinnerModule]
 })
 export class AppComponent {
   
