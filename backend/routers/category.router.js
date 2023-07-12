7f70bd5b4539f21f7a3b9e6d57a096d5789f6f0a
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-12 18:08:30 +0300

    Category service & model

--- backend/routers/category.router.js
+++ backend/routers/category.router.js
@@ -3,14 +3,13 @@ const router = express.Router();
 const Category = require("../models/category");
 const {v4:uuidv4} = require("uuid");
 
-
 router.post("/add", async(req,res)=>{
-    response(res, async()=> {
+    try {
         const {name} = req.body;
         
         const checkName = await Category.findOne({name: name});
         if(checkName != null){
-            res.status(403).json({message: "This category name is already in used!"});
+            res.status(403).json({message: "This category name is already in used"});
         }else{
             const category = new Category({
                 _id: uuidv4(),
@@ -18,42 +17,43 @@ router.post("/add", async(req,res)=>{
             });
     
             await category.save();
-            res.json({message: "Category registration is succeed!"});
+            res.json({message: "Category creation is succeed!"});
         }
-    });
-});
+        
+    } catch (error) {
+        res.stats(500).json({message: error.message});
+    }
+})
 
 router.post("/removeById", async(req, res)=>{
-    response(res, async ()=> {
+    try {
         const {_id} = req.body;
         await Category.findByIdAndRemove(_id);
         res.json({message: "Category is deleted"});
-    });
+    } catch (error) {
+        res.stats(500).json({message: error.message});
+    }
 });
 
 router.post("/update", async(req, res)=>{
-    response(res, async ()=> {
+    try {
         const {_id,name} = req.body;
         const category = await Category.findOne({_id:_id});
-
-        if(category.name != name){
-            const checkName = await Category.findOne({name: name});
-            if(checkName != null){
-                res.status(403).json({message: "This category name is already in used!"});
-            }else{
-                category.name = name;
-                await Category.findByIdAndUpdate(_id, category);
-                res.json({message: "Category is updated"});
-            }
-        }
-    });    
+        category.name = name;
+        await Category.findByIdAndUpdate(_id, category);
+        res.json({message: "Category is updated"});
+    } catch (error) {
+        res.stats(500).json({message: error.message});
+    }
 });
 
 router.get("/", async(req, res)=>{
-    response(res, async ()=> {
+    try {
         const categories = await Category.find().sort({name: 1});
         res.json(categories);
-    });
+    } catch (error) {
+        res.stats(500).json({message: error.message});
+    }
 });
 
 module.exports = router;
--- frontend/src/app/components/categories/categories.component.ts
+++ frontend/src/app/components/categories/categories.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CategoryModel } from './models/category.module';
+import { SharedModule } from 'src/app/common/shared/shared.module';
+import { NgForm } from '@angular/forms';
+import { CategoryService } from './services/category.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [CommonModule],
+  imports: [SharedModule],
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categories: CategoryModel[] = [];
+  constructor(
+    private _toastr: ToastrService,
+    private _category: CategoryService
+  ) { }
 
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll() {
+    this._category.getAll(res => this.categories = res);
+  }
+
+  add(form: NgForm) {
+    if (form.valid) {
+      this._category.add(form.controls["name"].value, res => {
+        this._toastr.success(res.message);
+        let element = document.getElementById("addModalCloseBtn");
+        element?.click();
+        form.reset();
+        this.getAll();
+      });
+    }
+  }
 }
