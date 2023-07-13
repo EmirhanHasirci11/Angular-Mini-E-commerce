f412dc2a6988284e39d25f59de78f6163d8dd8a9
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-13 21:28:55 +0300

    Order api

---
+++ frontend/src/app/components/baskets/components/baskets/baskets.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/common/shared/shared.module';
+import { ToastrService } from 'ngx-toastr';
+import { SwalService } from 'src/app/common/shared/services/swal.service';
+import { BasketModel } from '../../models/basket.model';
+import { BasketService } from '../../services/basket.service';
+
+@Component({
+  selector: 'app-baskets',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './baskets.component.html',
+  styleUrls: ['./baskets.component.css']
+})
+export class BasketsComponent implements OnInit {
+  baskets: BasketModel[] = [];
+  sum: number = 0;
+  
+  constructor(
+    private _basket: BasketService,
+    private _toastr: ToastrService,
+    private _swal: SwalService
+  ){}
+  
+    ngOnInit(): void {
+      this.getAll();
+    }
+  
+  getAll(){
+    this._basket.getAll(res=> {
+      this.baskets = res;
+      this.calculate();
+    });
+  }
+  
+  calculate(){
+    this.sum = 0;
+    this.baskets.forEach(element=> {
+      this.sum += (element.price * element.quantity)
+    });
+  }
+  
+  removeById(_id: string){
+    this._swal.callSwal("Do you want to remove this item form basket=","Remove Product","Remove",()=>{
+      let model = {_id: _id};
+      this._basket.removeById(model, res=> {
+        this._toastr.info(res.message);
+        this.getAll();
+      });
+    })  
+  }
+  }
